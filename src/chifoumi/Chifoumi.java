bdd322d5db475053897122edab5e0452f496635c
kyzblop <jeremymoi.jd@gmail.com>
2024-10-11 09:44:41 +0200

    debut

--- src/chifoumi/Chifoumi.java
+++ src/chifoumi/Chifoumi.java
@@ -1,9 +1,16 @@
 package chifoumi;
 
+import java.util.Scanner;
+
 public class Chifoumi {
 
 	public static void main(String[] args) {
-		System.out.println("Chifoumi");
+		System.out.println("Jeu du Chifoumi");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Saisir le nombre de partie");
+		
 
 	}
 
